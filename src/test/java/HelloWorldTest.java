d981c8bbe3a090da0acc225a82e6012a3eca8b53
Catherine Lee <catherine598616@gmail.com>
2018-10-06 15:42:15 -0700

    Correct test properly

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,7 +7,7 @@ public class HelloWorldTest {
 	public void testHelloWorld() {
 		HelloWorld helloWorld = new HelloWorld();
 		String result = helloWorld.getHellowWorld();
-		assertEquals(result, "Hello Worle");
+		assertEquals(result, "Hello World");
 	}
 	
 }
